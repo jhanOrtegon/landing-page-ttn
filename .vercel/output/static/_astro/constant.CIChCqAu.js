d7f8c879a77fd3d9d3fe512c602bb6461055e0d6
Jhan carlos ortegon <jhancarlos.ortegon@gmail.com>
2025-04-20 09:20:11 -0500

    feat: new changes

--- .vercel/output/static/_astro/constant.CIChCqAu.js
+++
@@ -1,1 +0,0 @@
-const t="gestionadmin@ttncompany.com",o="gerentedeproyectos@ttncompany.com",n="https://ttnstaging.biowel.com.co/comunicationms/api/v1/notifications/simpleEmail";export{n as A,t as J,o as a};
